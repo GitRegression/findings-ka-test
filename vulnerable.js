f2fabaed91b5cc190b6e5b87a458e48c2c64c256
Aditya Jha <aditya.narain.jha@armorcode.io>
2026-09-22 12:22:44 +0530

    Update vulnerable.js

--- vulnerable.js
+++ vulnerable.js
@@ -1,4 +1,22 @@
-app.get('/exec', (req, res) => {
-    eval(req.query.cmd);
-    require('child_process').exec(req.query.cmd);
+const express = require('express');
+  const app = express();
+  const { exec } = require('child_process');
+  
+  app.get('/exec', (req, res) => {
+    const userInput = req.query.cmd;
+    eval(userInput);
+    exec("ls " + userInput, (err, stdout) => {
+      res.send(stdout);
+    });
   });
+  
+  app.get('/redirect', (req, res) => {
+    res.redirect(req.query.url);
+  });
+  
+  app.get('/sql', (req, res) => {
+    const query = "SELECT * FROM users WHERE id = " + req.query.id;
+    res.send(query);
+  });
+  
+  app.listen(3000);
